22abbd8eb94f83fde00be235b7e61ad80bd6134b
ynwvroy <bennetfrmdao@gmail.com>
2023-08-14 15:00:42 -0700

    CPython

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -22,5 +22,6 @@ size_t print_listint(const lisstint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int is_palindrome(listint_t **head);
+void print_python_list_info(Pyobject *p);
 
 #endif /* LISTS_H */
