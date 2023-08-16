0182c54ce0c784e52e836688bf623c32bf8fe7aa
ynwvroy <bennetfrmdao@gmail.com>
2023-08-16 13:18:27 -0700

    Unique addition

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_numbers = set()
+    sum_result = 0
+
+    for num in my_list:
+        if num not in unique_numbers:
+            sum_result += num
+            unique_numbers.add(num)
+
+            return sum_result
