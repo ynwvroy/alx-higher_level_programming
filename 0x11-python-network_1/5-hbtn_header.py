8aef3d0c036ca5f0a8e999cbf34cd264fd66fb95
ynwvroy <bennetfrmdao@gmail.com>
2023-12-01 10:43:28 -0800

    *update*

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status
+You must use the actual package requests
+"""
+import requests
+import sys
+
+
+def main():
+    """fetches https://alx-intranet.hbtn.io/status
+    You must use the actual package requests
+    """
+    r = requests.get(sys.argv[1])
+    for key in r.headers:
+        if key == 'X-Request-Id':
+            val = r.headers['X-Request-Id']
+            print(val)
+            break
+
+
+if __name__ == "__main__":
+    main()
