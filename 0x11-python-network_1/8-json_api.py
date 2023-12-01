68b7cc354855846beba94bfbaa098b9d9137bfdd
ynwvroy <bennetfrmdao@gmail.com>
2023-12-01 10:50:47 -0800

    *update*

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""takes in a letter and also sends a POST request
+to http://0.0.0.0:5000/search_user with the letter
+as a parameter.
+"""
+import requests
+import sys
+
+
+def main():
+    """takes in a letter and also sends a POST request
+    to http://0.0.0.0:5000/search_user with the letter
+    as a parameter.
+    """
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) < 2:
+        data_load = {'q': ""}
+    else:
+        data_load = {'q': sys.argv[1]}
+
+    r = requests.post(url, data_load)
+
+    try:
+        r_dumped = r.json()
+    except ValueError as e:
+        print("Not a valid JSON")
+        return
+
+    if len(r_dumped) == 0:
+        print("No result")
+        return
+
+    print("[{}] {}".format(r_dumped['id'], r_dumped['name']))
+
+
+if __name__ == "__main__":
+    main()
