4299f642d7f39231a8869c5a8bbee585058b5d62
ynwvroy <bennetfrmdao@gmail.com>
2023-12-01 10:38:11 -0800

    *update*

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""takes in a URL, sending a request to the URL
+and displaying the body of the response (decoded in utf-8).
+
+You have to manage urllib.error.HTTPError exceptions
+and print: Error code: followed by the HTTP status code
+"""
+import sys
+import urllib.request
+import urllib.error
+
+
+def main():
+    """takes in a URL, sending a request to the URL
+    and displaying the body of the response (decoded in utf-8).
+
+    You have to manage urllib.error.HTTPError exceptions
+    and print: Error code: followed by the HTTP status code
+    """
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            this_page = response.read().decode('utf-8')
+            print(this_page)
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
+
+
+if __name__ == "__main__":
+    main()
